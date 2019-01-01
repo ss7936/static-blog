49c8e29d0497be614e2a413dd42abeda36383d6b
ss7936 <ssd7936@gmail.com>
2019-01-02 00:44:52 +0900

    main contents스타일을 layout으로 변경

---
+++ src/components/layout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../css/layout.css';
+
+export default ({ children }) => (
+	<div className="contents">
+		{children}
+	</div>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Link from 'gatsby-link';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Layout from '../components/layout';
 import '../css/home-main.css';
 
 export default () => (
 	<div className='wrapper'>
 		<Header />
-		<div className='contents'>
+		<Layout>
 			<p>welcome my blog</p>
 			<img src='https://source.unsplash.com/random/400x200' alt="" />
 			<Link to="/about/">About</Link>
-		</div>
+		</Layout>
 		{/* <Footer /> */}
 	</div>
 );
